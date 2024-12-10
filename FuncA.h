3d2dc4070112f72e733e4e5cc7e7e187fc31ef02
DmytroZor <zorenkodmytro03@gmail.com>
2024-12-10 11:58:10 +0000

    FuncA, main classes

---
+++ FuncA.h
@@ -0,0 +1,11 @@
+#ifndef FUNCA_H
+#define FUNCA_H
+
+class FuncA {
+public:
+    double calculate() {
+        return 0.0;
+    }
+};
+
+#endif 
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "FuncA.h"
+#include <iostream>
+
+int main() {
+    FuncA func;
+    std::cout << "Result: " << func.calculate() << std::endl;
+    return 0;
+}
